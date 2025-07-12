ab906c1368a0fc75d696f8e7d798800d32efcf59
HuyHao2512 <hohuyhao2013@gmail.com>
2025-07-12 12:43:25 +0700

    fix: Correct typo in ButtonLogin label in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
             <ButtonAccount />
           ) : (
             <Link to="/login">
-              <ButtonLogin label="Đăng nhập" />
+              <ButtonLogin label="Đăng nhậ" />
             </Link>
           )}
         </div>
